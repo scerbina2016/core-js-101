f01ef164c934ec6e1009dfd5d308df6aa8776715
Andrey Scerbina <scerbina2016@gmail.com>
2022-11-30 12:20:09 +0200

    feat: 01-strings-tasks all works

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -302,8 +302,12 @@ function encodeToRot13(str) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  const bol = false;
+  if ((typeof (value) === 'string') || (Object.prototype.toString.call(value).slice(8, -1) === 'String')) {
+    return true;
+  }
+  return bol;
 }
 
 
@@ -331,8 +335,13 @@ function isString(/* value */) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const arr = ['A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣',
+    'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦',
+    'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥',
+    'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠'];
+
+  return arr.indexOf(value);
 }
 
 
